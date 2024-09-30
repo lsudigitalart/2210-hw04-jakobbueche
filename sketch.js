33486d3d6cca7a300cf34dbbc857e098c3afd28d
“Your <jbuech8@lsu.edugit config --global user.email jbuech8@lsu.edu>
2024-09-30 12:34:38 -0500

    Here's what i have so far!

---
+++ sketch.js
@@ -0,0 +1,107 @@
+
+///////////////////
+// IN CLASS DEMO //
+///////////////////
+
+/* function setup () {
+
+    createCanvas(800,600)
+    noCursor()
+    circleSize = 50
+    sqx = 1300;
+    sqy = 200;
+    sqS = 200;
+
+}
+
+var circleSize;
+var sqx;
+var sqy;
+
+function draw () {
+
+    background(0)
+
+    fill(225)
+
+    ellipse(mouseX,mouseY,circleSize)
+
+    if(mouseX > sqx - circleSize/2 && mouseX < sqS*2 + circleSize/2) {
+        if(mouseY > sqy - circleSize/2 && mouseY < sqS*2 + circleSize/2) {
+          fill(255,255,0)    
+        }
+
+    }
+
+    square(sqx,sqy,sqS)
+   
+} */
+
+/////////////
+// MY GAME //
+/////////////
+
+let x = 20;
+let y = 200;
+let img
+let cWhite;
+let cBlack;
+
+function preload() {
+    img = loadImage('simplemazebam.jpg');
+}
+
+function setup() {
+  createCanvas(600, 600);
+//    cWhite = color(255, 255, 255, 255);
+   cWhite = [255, 255, 255, 255];
+
+   cBlack = color(0, 0, 0, 255);
+ 
+   c = [0, 0, 0, 255]
+  //image(img, 0, 0);
+
+  s = 10;
+}
+
+let c;
+
+function draw() {
+
+    image(img, 0, 0);
+    //background(0);
+
+  if (keyIsDown(LEFT_ARROW) === true) {
+    x -= 3;
+    c = get(x,y);
+    print(c)
+  }
+
+  if (keyIsDown(RIGHT_ARROW) === true) {
+    x += 3;
+    c = get(x,y);
+    print(c)
+  }
+
+  if (keyIsDown(UP_ARROW) === true) {
+    y -= 3;
+    c = get(x,y);
+    print(c)
+  }
+
+  if (keyIsDown(DOWN_ARROW) === true) {
+    y += 3;
+    c = get(x,y);
+    print(c)
+
+  }
+
+  noStroke();
+  fill(255);
+  circle(x, y, s);
+
+  if(c[0] == 255) {
+    print("out")
+  }
+
+}
